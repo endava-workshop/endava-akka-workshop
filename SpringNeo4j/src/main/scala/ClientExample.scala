455028109036a8caffc85f27ce4453e72ff0c5da
Ionuț Păduraru <ionut.c.paduraru@gmail.com>
2014-04-17 22:03:33 +0300

    Sample code to retrieve a web page (cherry picked from commit 2ac6f3a)

---
+++ SpringNeo4j/src/main/scala/ClientExample.scala
@@ -0,0 +1,25 @@
+import akka.actor.ActorSystem
+
+import scala.concurrent.Await
+import spray.httpx.encoding.Gzip
+import spray.client.pipelining._
+import scala.concurrent.duration._
+
+/**
+ * Sample code to retrieve a web page
+ */
+object ClientExample extends App {
+
+  implicit val system = ActorSystem()
+  import system.dispatcher // execution context for futures
+
+  val client = sendReceive ~> decode(Gzip)
+  val response = client(Get("http://spray.io/"))
+
+
+  val res = Await.result(response, 1 minute)
+  println(res)
+
+  system.shutdown()
+
+}
