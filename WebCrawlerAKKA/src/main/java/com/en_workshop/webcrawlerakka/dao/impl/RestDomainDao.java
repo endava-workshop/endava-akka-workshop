98a9af7a1b484db0c35f466c9db25c63f1c9da03
Ionuț Păduraru <ionut.c.paduraru@gmail.com>
2014-04-28 00:06:29 +0300

    [WebCrawler] removed debug values

--- WebCrawlerAKKA/src/main/java/com/en_workshop/webcrawlerakka/dao/impl/RestDomainDao.java
+++ WebCrawlerAKKA/src/main/java/com/en_workshop/webcrawlerakka/dao/impl/RestDomainDao.java
@@ -4,13 +4,7 @@ import com.en_workshop.webcrawlerakka.WebCrawlerConstants;
 import com.en_workshop.webcrawlerakka.dao.DomainDao;
 import com.en_workshop.webcrawlerakka.entities.Domain;
 import com.en_workshop.webcrawlerakka.rest.DomainURLClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 /**
@@ -19,7 +13,8 @@ import java.util.Set;
 public class RestDomainDao implements DomainDao {
 
     public List<Domain> findAll() {
-        return DomainURLClient.listDomains(5, 1000/*WebCrawlerConstants.DOMAINS_CRAWL_MAX_COUNT*/);
+//        return DomainURLClient.listDomains(5, 1000/*WebCrawlerConstants.DOMAINS_CRAWL_MAX_COUNT*/);
+        return DomainURLClient.listDomains(0, WebCrawlerConstants.DOMAINS_CRAWL_MAX_COUNT);
     }
 
 }
